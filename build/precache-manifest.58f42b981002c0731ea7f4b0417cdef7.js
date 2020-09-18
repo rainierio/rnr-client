13bc7a7c245147002ee33a8422339541fada1781
rainierio <rainieriletidjawa@gmail.com>
2020-09-18 23:45:21 +1000

    Initial build

---
+++ build/precache-manifest.58f42b981002c0731ea7f4b0417cdef7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed05f9daff9509404e6f476373898ad6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ae47816ab022a66a56e",
+    "url": "/static/css/2.81c53eb9.chunk.css"
+  },
+  {
+    "revision": "bd8084f0ca23f2bcf366",
+    "url": "/static/css/main.65561ca2.chunk.css"
+  },
+  {
+    "revision": "6ae47816ab022a66a56e",
+    "url": "/static/js/2.62cff885.chunk.js"
+  },
+  {
+    "revision": "bd8084f0ca23f2bcf366",
+    "url": "/static/js/main.f292b6b4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
