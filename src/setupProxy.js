908879faa54f33f5545b173356a539ab71b20034
rainierio <rainieriletidjawa@gmail.com>
2021-09-16 04:42:03 +1000

    update aws target url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://origin.eba-awajfifh.ap-southeast-2.elasticbeanstalk.com/',
+      target: 'http://rainierioapi-env-2.eba-zgumuuwf.ap-southeast-2.elasticbeanstalk.com//',
       changeOrigin: true,
     })
   );
