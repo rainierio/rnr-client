fb322a08e9e07e04dd90c3034d0e60a0085df15c
rainier letidjawa <rainierio@rainiers-MacBook-Pro-2.local>
2022-01-06 11:51:25 +0700

    re-build updated footer

---
+++ build/precache-manifest.09e96c796992a8be0bbe9d784ad17d1c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3108a9494852fb1eaf024fe377a154ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f412b794f3ff974af1bc",
+    "url": "/static/css/2.6c553b41.chunk.css"
+  },
+  {
+    "revision": "e879cb1e77126e6237c9",
+    "url": "/static/css/main.984caeae.chunk.css"
+  },
+  {
+    "revision": "f412b794f3ff974af1bc",
+    "url": "/static/js/2.53f0cd5a.chunk.js"
+  },
+  {
+    "revision": "edd2db0e0709d978880b040fea53ee1e",
+    "url": "/static/js/2.53f0cd5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e879cb1e77126e6237c9",
+    "url": "/static/js/main.8fd183d5.chunk.js"
+  },
+  {
+    "revision": "90c74ee14f36947426b3",
+    "url": "/static/js/runtime-main.f36081df.js"
+  },
+  {
+    "revision": "da35a2233260e2c4b4e9cc54859f7ce7",
+    "url": "/static/media/designer-avatar.da35a223.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
