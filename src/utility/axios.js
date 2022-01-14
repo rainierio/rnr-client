8d624e6c8d86514e208f58a3f8a978cfa06f08bc
rainier letidjawa <rainierio@rainiers-MacBook-Pro-2.local>
2022-01-14 22:27:40 +0700

    set axios credentials

--- src/utility/axios.js
+++ src/utility/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const instance = axios.create({ baseURL: 'http://3.13.224.4/' });
+const instance = axios.create({ baseURL: 'http://3.13.224.4/', withCredentials: false });
 
 export default instance
