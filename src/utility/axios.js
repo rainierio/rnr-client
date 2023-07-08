2da2019807526bf200af0955d6c17022cc69154f
Rainier Letidjawa <Rainierletidjawa@gmail.com>
2023-07-08 11:28:02 +0700

    update API endpoint:

--- src/utility/axios.js
+++ src/utility/axios.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 
+const localURL = process.env.LOCAL_API_ENDPOINT
+const productionURL = process.env.PRODUCTION_API_ENDPOINT
+const envstatus = process.env.NODE_ENV
+
+
+console.log(localURL, productionURL, envstatus);
 //const instance = axios.create({ baseURL: 'https://rainierio-api-svc.online/', withCredentials: false });
-const instance = axios.create({ baseURL: ' http://localhost:5000/', withCredentials: false });
+const instance = axios.create({ baseURL: 'https://rnr-server.vercel.app/', withCredentials: false });
+//const instance = axios.create({ baseURL: ' http://localhost:5000/', withCredentials: false });
 
 export default instance
